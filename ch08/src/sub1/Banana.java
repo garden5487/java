e1804b182be97f50a0aad8d0c55fc6b8064f224a
gardenpark9001 <gardenpark9001@gmail.com>
2025-01-15 11:28:51 +0900

    add ch08

---
+++ ch08/src/sub1/Banana.java
@@ -0,0 +1,22 @@
+package sub1;
+
+public class Banana {
+	
+	private String country;
+	private int price;
+	
+	public Banana(String country, int price) {
+		this.country = country;
+		this.price = price;
+		
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Apple [country=" + country + ", price=" + price + "]";
+	}
+
+	
+	
+}
